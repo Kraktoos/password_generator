3478f2bca38634f3308d4c747623d09e53ecaf07
Kraktoos <kraktoos@gmail.com>
2022-08-08 13:58:05 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const copyToClipboard = document.getElementById("password-btn");
+let last_password;
+
+const Characters = {
+  Uppers: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  Lowers: "abcdefghijklmnopqrstuvwxyz",
+  Numbers: "0123456789",
+  Symbols: "~`! @#$%^&*()_-+={[}]|:;\"'<,>.?/",
+};
+
+copyToClipboard.onmouseover = function () {
+  last_password = copyToClipboard.textContent;
+  copyToClipboard.textContent = "Copy to Clipboard";
+};
+
+copyToClipboard.onmouseout = function () {
+  copyToClipboard.textContent = last_password;
+  last_password = "";
+};
+
+copyToClipboard.addEventListener("click", function () {
+  navigator.clipboard.writeText(last_password);
+});
